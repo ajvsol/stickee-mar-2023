fe4915a42fdccfb33a30e96d16bddba3cf9bdf58
Aaron <22418751+ajvsol@users.noreply.github.com>
2023-03-07 13:20:09 +0000

    proj structure updated

---
+++ __tests__/index.test.js
@@ -0,0 +1,88 @@
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { sendPacks } from "../index.js";
+
+describe("sendPacks basic tests", () => {
+  it("should return an array of objects", () => {
+    const result = sendPacks(501);
+    expect(result).to.be.an("array");
+    expect(result[0]).to.be.an("object");
+  });
+
+  it("should return the correct number of packs for 1 widgets ordered", () => {
+    const result = sendPacks(1);
+    expect(result).to.deep.equal([{ 250: 1 }]);
+  });
+
+  it("should return the correct number of packs for 250 widgets ordered", () => {
+    const result = sendPacks(250);
+    expect(result).to.deep.equal([{ 250: 1 }]);
+  });
+
+  it("should return the correct number of packs for 251 widgets ordered", () => {
+    const result = sendPacks(251);
+    expect(result).to.deep.equal([{ 500: 1 }]);
+  });
+
+  it("should return the correct number of packs for 501 widgets ordered", () => {
+    const result = sendPacks(501);
+    expect(result).to.deep.equal([{ 500: 1 }, { 250: 1 }]);
+  });
+
+  it("should return the correct number of packs for 12001 widgets ordered", () => {
+    const result = sendPacks(12001);
+    expect(result).to.deep.equal([{ 5000: 2 }, { 2000: 1 }, { 250: 1 }]);
+  });
+});
+
+describe("sendPacks extended tests", () => {
+  it("should return the correct number of packs for 124 widgets ordered", () => {
+    const result = sendPacks(124);
+    expect(result).to.deep.equal([{ 250: 1 }]);
+  });
+
+  it("should return the correct number of packs for 376 widgets ordered", () => {
+    const result = sendPacks(376);
+    expect(result).to.deep.equal([{ 500: 1 }]);
+  });
+
+  it("should return the correct number of packs for 740 widgets ordered", () => {
+    const result = sendPacks(740);
+    expect(result).to.deep.equal([{ 500: 1 }, { 250: 1 }]);
+  });
+
+  it("should return the correct number of packs for 1441 widgets ordered", () => {
+    const result = sendPacks(1441);
+    expect(result).to.deep.equal([{ 1000: 1 }, { 500: 1 }]);
+  });
+
+  it("should return the correct number of packs for 2300 widgets ordered", () => {
+    const result = sendPacks(2300);
+    expect(result).to.deep.equal([{ 2000: 1 }, { 500: 1 }]);
+  });
+
+  it("should return the correct number of packs for 3141 widgets ordered", () => {
+    const result = sendPacks(3141);
+    expect(result).to.deep.equal([{ 2000: 1 }, { 1000: 1 }, { 250: 1 }]);
+  });
+
+  it("should return the correct number of packs for 5141 widgets ordered", () => {
+    const result = sendPacks(5141);
+    expect(result).to.deep.equal([{ 5000: 1 }, { 250: 1 }]);
+  });
+
+  it("should return the correct number of packs for 11141 widgets ordered", () => {
+    const result = sendPacks(11141);
+    expect(result).to.deep.equal([{ 5000: 2 }, { 1000: 1 }, { 250: 1 }]);
+  });
+
+  it("should return the correct number of packs for 21441 widgets ordered", () => {
+    const result = sendPacks(21441);
+    expect(result).to.deep.equal([{ 5000: 4 }, { 1000: 1 }, { 500: 1 }]);
+  });
+
+  it("should return the correct number of packs for 31441 widgets ordered", () => {
+    const result = sendPacks(31441);
+    expect(result).to.deep.equal([{ 5000: 6 }, { 1000: 1 }, { 500: 1 }]);
+  });
+});
---
+++ index.js
@@ -0,0 +1,47 @@
+export function sendPacks(orderSize) {
+  const packSizes = [5000, 2000, 1000, 500, 250];
+  const sentPacks = [];
+
+  let remainingOrderSize = orderSize;
+
+  for (let i = 0; i < packSizes.length; i++) {
+    // Add 1 of the current pack size if the remaining order size is greater than or equal to pack size
+    if (remainingOrderSize - packSizes[i] >= 0) {
+      // Only do this if the pack size is not already in sentPacks
+      if (sentPacks.length <= i) {
+        sentPacks.push({ [packSizes[i]]: 1 });
+        remainingOrderSize -= packSizes[i];
+      }
+    }
+
+    // If the remaining order size is greater than or equal to the current pack size, then update sentPacks
+    if (remainingOrderSize - packSizes[i] >= 0) {
+      if (`${packSizes[i]}` in sentPacks[i]) {
+        const count = Math.floor(remainingOrderSize / packSizes[i]);
+        sentPacks[i][packSizes[i]] = count + 1;
+        remainingOrderSize -= count * packSizes[i];
+      }
+    }
+  }
+
+  // If the remaining order size is more than 0 and less than the smallest pack size, then update sentPacks
+  if (
+    remainingOrderSize > 0 &&
+    remainingOrderSize < packSizes[packSizes.length - 1]
+  ) {
+    // if sentPacks is not empty & the smallest pack size is already in sentPacks
+    if (
+      sentPacks.length > 0 &&
+      parseInt(Object.keys(sentPacks[sentPacks.length - 1])[0]) ===
+        packSizes[packSizes.length - 1]
+    ) {
+      // then increase that pack size to the next smallest size
+      sentPacks.length = sentPacks.length - 1;
+      sentPacks.push({ [packSizes[packSizes.length - 2]]: 1 });
+    } else {
+      // otherwise just add a new smallest packSize to sentPacks
+      sentPacks.push({ [packSizes[packSizes.length - 1]]: 1 });
+    }
+  }
+  return sentPacks;
+}
